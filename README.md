8482f9aca8a25b52ac6f841fc773f7fe95be9df9
kchow23 <kchow423@gmail.com>
2018-07-25 22:35:30 -0400

    Update README

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 ## GridWorld Gym Environment
 **********
 
-GridWorld is a common MDP(Markov Decision Process) used in teaching AI and Reinforcement Learning. This is an environment you can import and implement basic algorithms.
+GridWorld is a common MDP (Markov Decision Process) used in teaching AI and Reinforcement Learning. This is an environment you can import and implement basic algorithms.
+
+![alt text](https://raw.githubusercontent.com/k--chow/gym_gridworld/master/gridworld.gif "Demo")
 
 ```
 git clone https://github.com/k--chow/gym_gridworld.git
 cd gym-gridworld
-pip install -e
+pip install -e .
 ```
 
 To use in code
@@ -16,6 +18,19 @@ import gym_gridworld
 
 env = gym.make('gridworld-v0')
 ```
+### Actions
+```
+action = 0 # move north
+action = 1 # move east
+action = 2 # move south
+action = 3 # move west
+```
 
-# TODO
-[ ] Add visual rendering
+### Observation Space
+This is a 3 x 4 grid.
+
+###
+Gridworld has a rock which is an invalid state, and two exit/game ending states (red and green), which return reward -1 and 1 respectively. MDP's are special because an intentional action is not deterministic; If we choose to go north (action 0), there is a 0.7 probability we go north, and a 0.15 probability we go in each orthogonal direction (0.15 east, 0.15 west).
+
+### TODO
+[x] Add visual rendering
--- gym_gridworld/envs/gridworld_env.py
+++ gym_gridworld/envs/gridworld_env.py
@@ -3,11 +3,13 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
 import random 
-
-
+from gym.envs.classic_control import rendering
 
 class GridWorldEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+  metadata = {
+    'render.modes': ['human', 'rgb_array'],
+    'video.frames_per_second': 50
+  }
 
   def __init__(self):
     self.action_space = spaces.Discrete(4)
@@ -17,6 +19,8 @@ class GridWorldEnv(gym.Env):
     self.seed()
     self.max_y = 2
     self.max_x = 3
+    self.viewer = None
+    self.rock = (1, 1)
   # invalid = -1, north = 0, east = 1, south = 2, west = 3
   def check_valid_step(self, action):
     x, y = self.state
@@ -62,6 +66,12 @@ class GridWorldEnv(gym.Env):
     else:
       new_x = x-1
       new_y = y
+    
+    # check if rock
+    if (new_x, new_y) == self.rock:
+        new_x = x
+        new_y = y
+
     if (new_x, new_y) == (3, 1) or (new_x, new_y) == (3, 2):
     	done = 1
     self.state = (new_x, new_y)
@@ -72,6 +82,48 @@ class GridWorldEnv(gym.Env):
     self.state = (3,0)
     return np.array(self.state)
 
+  def render(self, mode='human'):
+    x, y = self.state
+    screen_width = 400
+    screen_height = 300
+    robot_height = 20
+    robot_width = 10
+    offset = 50
+    grid_center_x = (x+1)*100 - offset
+    grid_center_y = (y+1)*100 - offset
+    if self.viewer is None:
+        self.viewer = rendering.Viewer(screen_width, screen_height)
+        rock = rendering.FilledPolygon([(1*100, 1*100), (1*100, 2*100), (2*100, 2*100), (2*100, 1*100)])
+        rock.set_color(.8,.6,.4)
+        green = rendering.FilledPolygon([(3*100, 1*100), (3*100, 2*100), (4*100, 2*100), (4*100, 1*100)])
+        green.set_color(0,1,0)
+        red = rendering.FilledPolygon([(3*100, 2*100), (3*100, 3*100), (4*100, 3*100), (4*100, 2*100)])
+        red.set_color(1,0,0)
+        self.viewer.add_geom(rock)
+        self.viewer.add_geom(green)
+        self.viewer.add_geom(red)
+    l,r,t,b = grid_center_x - robot_width/2, grid_center_x + robot_width/2, grid_center_y + robot_height/2, grid_center_y - robot_height/2
+    robot = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+    #self.robottrans = rendering.Transform()
+    #robot.add_attr(self.robottrans)
+    self.viewer.add_onetime(robot)
+
+    if self.state is None:
+        return None
+    self.viewer.draw_line((0, 100), (screen_width, 100))
+    self.viewer.draw_line((0, 200), (screen_width, 200))
+    self.viewer.draw_line((100, 0), (100, screen_height))
+    self.viewer.draw_line((200, 0), (200, screen_height))
+    self.viewer.draw_line((300, 0), (300, screen_height))
+    #self.robottrans.set_translation(-100, 100)
+
+
+    return self.viewer.render(return_rgb_array = mode=='rgb_array')
+
+  def close(self):
+    if self.viewer: self.viewer.close()
+
   def seed(self, seed=None):
     self.np_random, seed = seeding.np_random(seed)
     return [seed]
+
